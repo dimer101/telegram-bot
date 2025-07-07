893689672e1d2f20c2885e7d65980011dd7e0dc5
dimer101 <110110255+dimer101@users.noreply.github.com>
2025-07-07 04:27:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text("▀▄▀▄▀▄🐔 【S】𝕙卂爪🅰𝓲ⓒⓗ ꋖ𝔢ꌅ𝓇𝔦[̲̅t]𝑜[̲̅r]【y】 🐔▄▀▄▀▄▀", reply_markup=reply_markup)
-    await update.message.reply_photo(photo="blob:https://web.telegram.org/af61e291-7c6e-41c4-bd8d-8085c6d976ab")
+    await update.message.reply_photo(photo="https://i.postimg.cc/fy7h0Wgj/temp-Image0hc-CGf.avif")
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
